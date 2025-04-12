437855210248a2019026719f0dc9482a6947d944
shivank-mittal <mittal.shivank@gmail.com>
2025-04-12 21:03:37 +0200

    fix: wrong  base url

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     geminiKey: "AIzaSyCgi4UT9risn5UYU9mcY0SxbLoZlWx6mrw",
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: "https://ai-chat-python.vercel.app"
   };
